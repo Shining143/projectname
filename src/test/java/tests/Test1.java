a9b7d41f23461575d60944a3b65af3cff66e0efe
Shining143 <ahmadslb2293@gmail.com>
2023-07-03 09:55:15 +0530

    I have change code

--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -7,12 +8,15 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Test1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		WebDriverManager.chromedriver().setup();
 		RemoteWebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.gmail.com");
+		Thread.sleep(3000);
+		driver.findElement(By.name("identifier")).sendKeys("jp9088768@gmail.com");
+		
 		
 
 	}
